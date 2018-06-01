e11830bb1e93986dcf6ca27e03b54baf1cfb0197
Josef Baltz <josef@baltz.net>
2018-06-01 15:46:14 -0500

    Added 'mom' detection

--- src/main/java/me/orangeflare/mombot/responder.java
+++ src/main/java/me/orangeflare/mombot/responder.java
@@ -8,9 +8,9 @@ import sx.blah.discord.util.RequestBuffer;
 public class responder {
     @EventSubscriber
     public void onMessageReceived(MessageReceivedEvent event) {
-        String formattedMsg = event.getMessage().getContent();
+        String formattedMsg = event.getMessage().getContent().toLowerCase();
         if (formattedMsg.contains("?")) {
-            if (event.getAuthor().getLongID() == 176108182056206336L || event.getAuthor().getLongID() == 253733469778083840L || event.getAuthor().getLongID() == 241685759348703232L) {
+            if (event.getAuthor().getLongID() == 176108182056206336L) {
                 System.out.println("[INFO] Not sending 'Ask your Father'");
                 System.out.println("    Reason: " + event.getAuthor().getDisplayName(event.getGuild()) + " Sent Message");
             } else if (event.getMessage().getAuthor().isBot()){
@@ -22,6 +22,23 @@ public class responder {
                         event.getChannel().sendMessage("Ask your father");
                     } catch (DiscordException error) {
                         System.err.println("[ERROR] Could Not Send 'Ask your Father'");
+                        error.printStackTrace();
+                    }
+                });
+            }
+        } else if (formattedMsg.contains("mom")) {
+            if (event.getAuthor().getLongID() == 176108182056206336L) {
+                System.out.println("[INFO] Not sending 'Not now Sweetie'");
+                System.out.println("    Reason: " + event.getAuthor().getDisplayName(event.getGuild()) + " Sent Message");
+            } else if (event.getMessage().getAuthor().isBot()){
+                //do nothing
+            } else {
+                RequestBuffer.request(() -> {
+                    try {
+                        System.out.println("[INFO] Sending 'Not now Sweetie' at " + event.getAuthor().getDisplayName(event.getGuild()));
+                        event.getChannel().sendMessage("Not now sweetie");
+                    } catch (DiscordException error) {
+                        System.err.println("[ERROR] Could Not Send 'Not now Sweetie'");
                         error.printStackTrace();
                     }
                 });
